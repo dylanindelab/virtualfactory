e3bba8f3a5eb8dfd69bcbd37094191578faee385
unknown <dylan@indelab.fr>
2020-02-18 21:28:04 +0100

    ajout du bluescreen sur le fichier test.php

--- js/app.js
+++ js/app.js
@@ -1,84 +1,69 @@
-//  var makerjs = require('makerjs'); // Library by Microsott for laser cuter create dxf files(Inutile pour le moment)
+
 jQuery(document).ready(function($) {
-
     console.log('Document is ready') // Affice dans la console la phrase le document est prêt des que le document est prêt.
+    const sbp = require("svg-blueprint");
 
-  $( "#square_button" ).click(function() { // Dès que l'on clique sur le bouton carré les paramètres de celui ci apparaisent.
+
+    $( "#square_button" ).click(function() { // Dès que l'on clique sur le bouton carré les paramètres de celui ci apparaisent.
         $('.square_size').toggleClass('show'); // On rajoute une class a notre class pour pouvoir la masquer ou l'ajouter.
-  });
+    });
 
-  let canvas = new fabric.Canvas('myCanvas'); // Je fabrique un nouveau canvas grâce a ma balise canvas prévu dans mon html
-  canvas.setDimensions({width:640, height:500}); // Je prévois des dimension par defaut a mon cavnas
+    let canvas = new fabric.Canvas('myCanvas'); // Je fabrique un nouveau canvas grâce a ma balise canvas prévu dans mon html
+    canvas.setDimensions({width:640, height:500}); // Je prévois des dimension par defaut a mon cavnas
 
-$("#validate_square").click(function(){ // Lorsque l'on clique sur le bouton avec l'id  #valide dans le html
-  let htrinput = $("#hauteurinput").val(); // On récupere la valeur taper dans le input hauteur. Brut en milimetre et non relatif (ce qui et afficher a l'écran)
-  let lrginput = $("#largeurinput").val(); // On récupere la valeur taper dans le input largeur. Brut en milimetre et non relatif (ce qui et afficher a l'écran)
+  $("#validate_square").click(function(){ // Lorsque l'on clique sur le bouton avec l'id  #valide dans le html
+    let htrinput = $("#hauteurinput").val(); // On récupere la valeur taper dans le input hauteur. Brut en milimetre et non relatif (ce qui et afficher a l'écran)
+    let lrginput = $("#largeurinput").val(); // On récupere la valeur taper dans le input largeur. Brut en milimetre et non relatif (ce qui et afficher a l'écran)
   // alert(htrinput + " " + lrginput); // J'affiche une alert avec la hauteur taper et la largeur taper a son tour
 
-  if(htrinput <= 0 || lrginput <= 0) {
-    alert('Désolé mais les paramètre inserez ne sont pas valide.');
-  } else {
+    if(htrinput <= 0 || lrginput <= 0) {
+      alert('Désolé mais les paramètre inserez ne sont pas valide.');
+    } else {
+      let rect = new fabric.Rect({ // J'utilise la variable let car ma variables et destinée a changer en fonction des inputs du client.
+      left: 320, // La position initial gauche de la géometrie afficher sur l'écran
+      top: 250, // La position initial haut de la géometrie affichier sur l'écran
+      // stroke: 'red',
+      fill: 'transparent',
+      stroke: 'red', // La couleur de la géometrie
+      width: 40,   // La largeur qui correspond a ce que a entrer le client dans sont champs input
+      height: 40, // La hauteur qui correspond a ce que a entrer le client dans sont champs input
+      hoverCursor: 'true',
+    });
+canvas.add(rect);
+this.rect;
+
+localStorage.setItem("largeur", lrginput); // Stocke en données local chez le client la largeur entré par l'utilisateur.
+localStorage.setItem("hauteur", htrinput); // Stocke en données local chez le client la hauteur entré par l'utilisateur.
+
   
-  let rect = new fabric.Rect({ // J'utilise la variable let car ma variables et destinée a changer en fonction des inputs du client.
-  left: 320, // La position initial gauche de la géometrie afficher sur l'écran
-  top: 250, // La position initial haut de la géometrie affichier sur l'écran
-  // stroke: 'red',
-  fill: 'transparent',
-  stroke: 'red', // La couleur de la géometrie
-  width: 40,   // La largeur qui correspond a ce que a entrer le client dans sont champs input
-  height: 40, // La hauteur qui correspond a ce que a entrer le client dans sont champs input
-  hoverCursor: 'true',
-});
+let cookielargeur = localStorage.getItem("largeur"); // Récupère la donnée local pour la largeur.
+let cookiehauteur = localStorage.getItem("hauteur"); // Récupère la donnée local pour la hauteur
+let largeursetting = document.getElementById("lrg_setting");
+let hauteursetting = document.getElementById("htr_setting");
 
-
-  canvas.add(rect);
-
-
-
-  let newsize = rect.getScaledWidth();
-  console.log(newsize);
-
-  // Et à ce moment est la le canvas créer et ajoute le rect(rectangle ou carré en fonction de l - h que a entrer le client dans les parametre)
-
-  this.rect;
-
-  // alert('Vos dimensions: Largeur : ' + lrginput + ' mm, Hauteur : ' + htrinput + ' mm.');
-  localStorage.setItem("largeur", lrginput); // Stocke en données local chez le client la largeur entré par l'utilisateur.
-  localStorage.setItem("hauteur", htrinput); // Stocke en données local chez le client la hauteur entré par l'utilisateur.
-
-  let cookielargeur = localStorage.getItem("largeur"); // Récupère la donnée local pour la largeur.
-  let cookiehauteur = localStorage.getItem("hauteur"); // Récupère la donnée local pour la hauteur
-
-   // Récupere le champs htr_setting dans mon html
-
-  let largeursetting = document.getElementById("lrg_setting");
-  let hauteursetting = document.getElementById("htr_setting");
-
-  canvas.on("object:modified", function (e) {
-    var activeObject = e.target;
-        if (!activeObject) {
-            return;
+canvas.on("object:modified", function (e) {
+  let activeObject = e.target;
+    if (!activeObject) {
+      return;
     }
-    var newWidth = activeObject.width * activeObject.scaleX ;
-    var newHeight = activeObject.height * activeObject.scaleY ;
-
-    activeObject.width = newWidth;
-    activeObject.height = newHeight;
-
-    activeObject.scaleX = 1;  
-    activeObject.scaleY = 1;  
-
-    // refler le soucis de réglage de hauteur.     
-    
-    console.log(newWidth * cookielargeur / 40); // Console.log de mon calcule pour la conversion milimètres
-
-  let largeurenmm = parseFloat(newWidth * cookielargeur / 40).toPrecision(6);
-  let hauteurenmm = parseFloat(newHeight * cookiehauteur / 40).toPrecision(6);
   
-  $(largeursetting).html("Largeur de votre géometrie: " + largeurenmm + 'mm'); // Ajoute dans mon html les données stocké en local.
-  $(hauteursetting).html("Hauteur de votre géometrie: " + hauteurenmm + 'mm'); // Ajoute dans mon html les données stocké en local.
+let  newWidth = activeObject.width * activeObject.scaleX ;
+let newHeight = activeObject.height * activeObject.scaleY ;
+activeObject.width = newWidth;
+activeObject.height = newHeight;
+activeObject.scaleX = 1;  
+activeObject.scaleY = 1;  
+
+let largeurenmm = parseFloat(newWidth * cookielargeur / 40).toPrecision(6);
+let hauteurenmm = parseFloat(newHeight * cookiehauteur / 40).toPrecision(6);
+  
+$(largeursetting).html("Largeur de votre géometrie: " + largeurenmm + 'mm'); // Ajoute dans mon html les données stocké en local.
+$(hauteursetting).html("Hauteur de votre géometrie: " + hauteurenmm + 'mm'); // Ajoute dans mon html les données stocké en local.
 });
     }
   })
+
+
+
 }) // End of document.ready (JQuery)
  
