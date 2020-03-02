81428d632ee0d797ee7c4d6a1b98b1e765f231b5
jordan bourbotte <dylan@indelab.fr>
2020-03-02 09:44:05 +0100

    Premier essaie de création autre que le 'square'

--- js/test.js
+++ js/test.js
@@ -2,11 +2,11 @@ jQuery(document).ready(function($) {
 
   /* Ajout d'une variable pour l'écran bleu (inspiré sur maker.js.org), cette variable peut prendre en compte plusieurs
      paramètres, pour voir la liste des paramètre rendez vous sur la documentation */
-    let blueprint = new svgBlueprint.Blueprint({ 
-    parentSelector: "canvas",
-    width: '100%',
-    height: '100%'
-    });
+    // let blueprint = new svgBlueprint.Blueprint({ 
+    // parentSelector: "canvas",
+    // width: '100%',
+    // height: '100%'
+    // });
 
     /* Initialisation de mon canvas */
     var canvasEl = document.getElementById('myCanvas');
@@ -23,11 +23,17 @@ jQuery(document).ready(function($) {
         $('.select_shape').toggleClass('active');
       });
 
+
+
+
     /* Pour chaque une des formes présente dans la barre de choix (premier select) nous allons ajoutez un class active, ce qui nous
     permettera plus tard, de savoir quelle forme et actuellement en cours d'édition */
     $('.shape').on('click', function(){
       $('.setting_pattern').toggleClass('active');
     })
+
+
+
 
     $('.square_shape').on('click', function(){
       $('.submitshape').on('click', function(){
@@ -35,8 +41,8 @@ jQuery(document).ready(function($) {
           left: 100,
           top: 100,
           fill: 'red',
-          width: 10,  // Les dimension par default sont de 30px par 30px (relative)
-          height: 10, // Les dimension par default sont de 30px par 30px (relative)
+          width: 40,  // Les dimension par default sont de 30px par 30px (relative)
+          height: 40, // Les dimension par default sont de 30px par 30px (relative)
           angle: 45
         });
 
@@ -46,8 +52,8 @@ jQuery(document).ready(function($) {
           let widthscale = parseInt(this.getScaledWidth())
           let heightscale = parseInt(this.getScaledHeight())
           
-          console.log("Largeur de : " + widthscale);
-          console.log("Hauteur de : " + heightscale);
+          // console.log("Largeur de : " + widthscale);
+          // console.log("Hauteur de : " + heightscale);
 
           let htrinput = $("#hauteurinput").val(); // On récupere la valeur taper dans le input hauteur. Brut en milimetre et non relatif (ce qui et afficher a l'écran)
           let lrginput = $("#largeurinput").val();
@@ -69,23 +75,40 @@ jQuery(document).ready(function($) {
           $(largeursetting).html("Largeur de votre géometrie: " + largeurenmm + 'mm'); // Ajoute dans mon html les données stocké en local.
           $(hauteursetting).html("Hauteur de votre géometrie: " + hauteurenmm + 'mm'); // Ajoute dans mon html les données stocké en local.         
           $(surfacesetting).html("Surface de votre géometrie: " + surfaceenmm + 'mm'); // Ajoute dans mon html les données stocké en local.         
-         })
+         });
     
       })
     })
+
+
+
 
 
     $('.circle_shape').on('click', function(){
       alert('Vous avais cliquez sur la cercle');
     })
 
+
+
+
+
+
     $('.triangle_shape').on('click', function(){
       alert('Vous avais cliquez sur la triangle');
     })
 
+
+
+
+
+
     $('.draw_shape').on('click', function(){
       alert('Vous avais cliquez sur le dessin libre');
     })
+
+
+
+    
 
 
  }); 
